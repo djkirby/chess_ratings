1d9bac22aba186835827b9c4f8794de652d20cea
djkirby <djkirby@wpi.edu>
2014-02-14 19:33:29 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChessRatings::Application.config.secret_key_base = 'a2efdaacd13f85e28481e2757bb79c47c350239c5709ad3f5de4ab9668e00d79197c5bcc6b77d56f5b78439d480b2be8867cd92400fd47785311eee55936d5f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChessRatings::Application.config.session_store :cookie_store, key: '_chess_ratings_session'
